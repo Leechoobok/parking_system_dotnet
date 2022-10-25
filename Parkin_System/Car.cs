c1b76bc0bed705ba389ee4586bc0aa32a4d97fdc
disze84 <disze84@gmail.com>
2022-10-25 22:23:39 +0900

    dotnet

---
+++ Parkin_System/Car.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media.Animation;
+using System.Xml.Schema;
+
+
+
+namespace Parking_System
+{
+    public class Car
+    {
+        string spot;
+        string license_plate;
+        int into_ptimeH, into_ptimeM;
+        int fare;
+        List<string> default_spot;
+        List<string> default_plate;
+        public Car(string spot, string license_plate, int into_ptimeH, int into_ptimeM)
+        {
+            this.spot = spot;
+            this.license_plate = license_plate;
+            this.into_ptimeH = into_ptimeH;
+            this.into_ptimeM = into_ptimeM;
+            this.default_spot = new List<string>();
+            this.default_plate = new List<string>();
+
+        }
+        public int Rate_check()
+        {
+            int chk_timeA, chk_timeB;
+            DateTime time = DateTime.Now;
+            chk_timeA = (time.Hour * 60) + time.Minute;
+            chk_timeB = (this.into_ptimeH * 60) + this.into_ptimeM;
+            this.fare = (chk_timeA - chk_timeB) / 10 * 800;
+            return fare;
+
+        }
+        public void Set_car(string license_plate)
+        {
+            DateTime time = DateTime.Now;
+            this.license_plate = license_plate;
+            this.into_ptimeH = time.Hour;
+            this.into_ptimeM = time.Minute;
+        }
+        public void Set_spot(string spot)
+        {
+            this.spot = spot;
+        }
+        public string Get_spot()
+        {
+            return this.spot;
+        }
+        public string GetLicense_plate()
+        {
+            string str;
+            str = this.license_plate;
+            return str;
+        }
+        public int Getinto_timeH()
+        {
+            int into_t_h;
+            into_t_h = this.into_ptimeH;
+            return into_t_h;
+        }
+        public int Getinto_timeM()
+        {
+            int into_t_m;
+            into_t_m = this.into_ptimeM;
+            return into_t_m;
+        }
+        public int Getparking_time()
+        {
+            int chk_time, chk_timeA, chk_timeB;
+            DateTime time = DateTime.Now;
+            chk_timeA = (time.Hour * 60) + time.Minute;
+            chk_timeB = (this.into_ptimeH * 60) + this.into_ptimeM;
+            chk_time = chk_timeA - chk_timeB;
+            return chk_time;
+        }
+        public void WriteData()
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream("parking_car.dat", FileMode.Append)))
+            {
+                bw.Write(this.spot);
+                bw.Write(this.license_plate);
+                bw.Write(this.into_ptimeH);
+                bw.Write(this.into_ptimeM);
+                bw.Write("\n");
+            }
+            this.default_spot.Add(this.spot);
+            this.default_plate.Add(this.license_plate);
+
+        }
+        public int ReadData(string str)
+        {
+            FileStream fs = File.Open("parking_car.dat", FileMode.Open, FileAccess.Read);
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                while (fs.Length != fs.Position)
+                {
+                    string spot = br.ReadString();
+                    string plate = br.ReadString();
+                    int into_ptimeH = br.ReadInt32();
+                    int into_ptimeM = br.ReadInt32();
+                    string n_line = br.ReadString();
+                    this.spot = spot;
+                    this.license_plate = plate;
+                    this.into_ptimeH = into_ptimeH;
+                    this.into_ptimeM = into_ptimeM;
+                    if (this.license_plate == str) break;
+                    //Console.Write(s1 + " " + " " + s2 + " " + " " + i1 + " " + i2 + " " + " " + s3);
+                }
+            }
+            fs.Close();
+            if (this.license_plate == str) return 1;
+            else return 0;
+        }
+        public void ReadData()
+        {
+            FileStream fs = File.Open("parking_car.dat", FileMode.OpenOrCreate, FileAccess.Read);
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                while (fs.Length != fs.Position)
+                {
+                    string spot = br.ReadString();
+                    string plate = br.ReadString();
+                    int into_ptimeH = br.ReadInt32();
+                    int into_ptimeM = br.ReadInt32();
+                    string n_line = br.ReadString();
+                    this.spot = spot;
+                    this.license_plate = plate;
+                    this.into_ptimeH = into_ptimeH;
+                    this.into_ptimeM = into_ptimeM;
+                    this.default_plate.Add(this.license_plate);
+                    this.default_spot.Add(this.spot);
+                    //Console.Write(s1 + " " + " " + s2 + " " + " " + i1 + " " + i2 + " " + " " + s3);
+                }
+            }
+            fs.Close();
+            if(this.default_spot.ElementAt(0) == "")
+            {
+                this.license_plate = "";
+                this.default_plate.RemoveAt(default_plate.Count - 1);
+                this.default_spot.RemoveAt(default_spot.Count - 1);
+                return;
+            }
+        }
+        public int Get_Count()
+        {
+            return this.default_plate.Count();
+        }
+        public string Get_default_Sport(int i)
+        {
+            return this.default_spot.ElementAt(i);
+        }
+        public string Get_default_License_plate(int i)
+        {
+            return this.default_plate.ElementAt(i);
+        }
+        public void DeleteData(string str)
+        {
+            FileStream fs1 = File.Open("parking_car.dat", FileMode.Open, FileAccess.Read);
+            FileStream fs2 = File.Open("tmp.dat", FileMode.Create, FileAccess.Write);
+            using (BinaryReader br = new BinaryReader(fs1))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs2))
+                {
+                    while (fs1.Length != fs1.Position)
+                    {
+                        string spot = br.ReadString();
+                        string plate = br.ReadString();
+                        int into_ptimeH = br.ReadInt32();
+                        int into_ptimeM = br.ReadInt32();
+                        string n_line = br.ReadString();
+                        this.spot = spot;
+                        this.license_plate = plate;
+                        this.into_ptimeH = into_ptimeH;
+                        this.into_ptimeM = into_ptimeM;
+
+                        if (this.license_plate == str)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            bw.Write(this.spot);
+                            bw.Write(this.license_plate);
+                            bw.Write(this.into_ptimeH);
+                            bw.Write(this.into_ptimeM);
+                            bw.Write("\n");
+                        }
+                    }
+                }
+            }
+            fs2.Close();
+            fs1.Close();
+            File.Delete("parking_car.dat");
+            File.Move("tmp.dat", "parking_car.dat");
+            for(int i = 0;i<default_plate.Count;i++)
+            {
+                if(str == this.default_plate.ElementAt(i))
+                {
+                    this.default_spot.RemoveAt(i);
+                    this.default_plate.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
---
+++ Parkin_System/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.Win32;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tesseract;
+
+namespace Parking_System
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        Car live_car = new Car("spot", "test", 0, 0);
+        
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+            
+        }
+        void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Label[] labels = new Label[] { P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31, P32 };
+            string tmp_spot;
+            live_car.ReadData();
+            for (int i = 0; i < live_car.Get_Count(); i++)
+            {
+                tmp_spot = live_car.Get_default_Sport(i);
+                labels[int.Parse(tmp_spot.Substring(1))].BorderBrush = new SolidColorBrush(Colors.Blue);
+                labels[int.Parse(tmp_spot.Substring(1))].Background = new SolidColorBrush(Colors.White);
+                labels[int.Parse(tmp_spot.Substring(1))].Foreground = new SolidColorBrush(Colors.Blue);
+                labels[int.Parse(tmp_spot.Substring(1))].Content = live_car.Get_default_License_plate(i);
+            }
+        }
+        private void into_parking_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Label[] labels = new Label[] { P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31, P32 }; 
+            OpenFileDialog open = new OpenFileDialog();
+            string dir;
+            string plate="", spot;
+            string str_data = "０１２３４５６７８９";
+            string str_data1 = "0123456789";
+            open.Filter = "PNG파일 (*.PNG, *.png) | *.PNG;*.png;";
+            
+            list_Status.Items.Clear();
+            list_Status.IsEnabled = false;
+            leave_parking_btn.IsEnabled = false;
+            lbl_title.Content = "";
+      
+            if(open.ShowDialog() == true)
+            {
+                dir = open.FileName.ToString();
+            }else  return;
+            
+            Bitmap bitmap = new Bitmap(open.FileName);
+            //Pix pix = PixConverter.ToPix(bitmap);
+            var ocr = new TesseractEngine("./tessdata", "kor", EngineMode.Default);
+            var texts = ocr.Process(bitmap);
+            string outText = texts.GetText();
+            ocr.Dispose();
+            Debug.WriteLine(outText);
+            for (int i=0;i<outText.Length;i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    if (outText[i] == str_data[j]) plate += str_data1[j];
+                    else if (outText[i] == str_data1[j]) plate += str_data1[j];
+                   
+                }
+                if (0xAC00 <= outText[i] && outText[i] <= 0xD7A3) plate += outText[i];
+            }
+            Input_Text.Text = plate;
+
+            for (int i = 0; i < live_car.Get_Count(); i++)
+            {
+                if (plate == live_car.Get_default_License_plate(i))
+                {
+                    MessageBox.Show("이미 입차된 차량입니다.", "오류");
+                    return;
+
+                }
+            }
+            ////////////////////////////////////////////////////////////////////////////////
+            live_car.Set_car(plate);
+            Random random = new Random();
+            List<int> list = new List<int>();
+            for(int i=0; i<33;i++)
+            {
+                if (!(labels[i].Content.ToString()==""))
+                {
+                    list.Add(i);
+                }
+            }
+
+            int randomspot;
+
+            randomspot = random.Next(33);
+            while (list.Contains(randomspot))
+                randomspot = random.Next(33);
+
+            list.Add(randomspot);
+            spot = labels[list[list.Count - 1]].Name.ToString();
+            live_car.Set_spot(spot);
+            Debug.WriteLine(list[list.Count - 1]);
+            labels[list[list.Count - 1]].BorderBrush = new SolidColorBrush(Colors.Blue);
+            labels[list[list.Count - 1]].Background = new SolidColorBrush(Colors.White);
+            labels[list[list.Count - 1]].Foreground = new SolidColorBrush(Colors.Blue);
+            labels[list[list.Count - 1]].Content = plate;
+            live_car.WriteData();
+
+        }
+
+        private void search_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+            string tmp = Input_Text.Text;
+            int r_data;
+            if (string.IsNullOrEmpty(tmp))
+            {
+                MessageBox.Show("차량번호를 입력해 주세요", "오류");
+
+            }
+            else
+            {
+
+                r_data = live_car.ReadData(tmp);
+                if (r_data == 1)
+                {
+                    imagebox.Source = null;
+                    list_Status.Items.Clear();
+                    list_Status.IsEnabled = true;
+                    leave_parking_btn.IsEnabled = false;
+                    rate_check_btn.IsEnabled = true;
+
+                    lbl_title.Content = "차량번호\t\t\t입차시간";
+                    list_Status.Items.Add(live_car.GetLicense_plate() + "\t\t\t\t" + live_car.Getinto_timeH() + "시" + live_car.Getinto_timeM() + "분");
+                }
+                else
+                {
+                    MessageBox.Show("주차된 차량이 없습니다.", "오류");
+                }
+            }
+        }
+
+        private void rate_check_btn_Click(object sender, RoutedEventArgs e)
+        {
+            imagebox.Source = null;
+            list_Status.Items.Clear();
+            list_Status.IsEnabled = true;
+            leave_parking_btn.IsEnabled = true;
+            rate_check_btn.IsEnabled = false;
+            lbl_title.Content = "차량번호\t\t\t이용시간\t\t\t이용요금";
+            list_Status.Items.Add(live_car.GetLicense_plate() + "\t\t\t\t" + live_car.Getparking_time() + "\t\t\t\t" + live_car.Rate_check());
+        }
+
+        private void leave_parking_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Label[] labels = new Label[] { P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31, P32 };
+            string tmp = Input_Text.Text;
+
+            labels[int.Parse(live_car.Get_spot().Substring(1))].BorderBrush = new SolidColorBrush(Colors.White);
+            labels[int.Parse(live_car.Get_spot().Substring(1))].Background = null;
+            labels[int.Parse(live_car.Get_spot().Substring(1))].Foreground = new SolidColorBrush(Colors.Black);
+            labels[int.Parse(live_car.Get_spot().Substring(1))].Content = "";
+
+            list_Status.Items.Clear();
+            leave_parking_btn.IsEnabled = false;
+            list_Status.IsEnabled = false;
+            rate_check_btn.IsEnabled = false;
+            live_car.DeleteData(tmp);
+        }
+
+        private void end_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+    }
+}
